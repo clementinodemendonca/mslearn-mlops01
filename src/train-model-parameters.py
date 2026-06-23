2f05b9d7fc647ce456d92308afd99c72e3de36db
clementinodemendonca <clementinodemendonca@users.noreply.github.com>
2026-06-23 21:34:54 +0000

    Adjust training parameters

--- src/train-model-parameters.py
+++ src/train-model-parameters.py
@@ -11,6 +11,7 @@ from sklearn.metrics import roc_curve
 import matplotlib.pyplot as plt
 
 #documentaion needed
+# yet another documentation needed
 def main(args):
     # read data
     df = get_data(args.training_data)
